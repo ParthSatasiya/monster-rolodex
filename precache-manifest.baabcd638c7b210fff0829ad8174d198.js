35888744b883f377aea38697e7fe9c32d16286cd
Parth <parth@Users-MacBook-Pro.local>
2020-02-02 19:40:06 +0530

    Updates

---
+++ precache-manifest.baabcd638c7b210fff0829ad8174d198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51855f4c321088248ba3fdc427133c9",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "9b63f93421904da8873a",
+    "url": "/monster-rolodex/static/css/main.26e3b008.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b63f93421904da8873a",
+    "url": "/monster-rolodex/static/js/main.77f51f06.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
